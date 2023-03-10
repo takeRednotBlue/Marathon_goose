e765131401614fac94222ed28bbe2f8ca884c6f9
takeRednotBlue <127436225+takeRednotBlue@users.noreply.github.com>
2023-03-10 21:26:18 +0200

    Scale, game over, goose
    Changed scale of the objects, moved animation img to a folder and rewrite their load using list comprehension. Made a game over title to appear after colliding with enemy and quiting the game.

--- main.py
+++ main.py
@@ -1,12 +1,14 @@
 import random
 import pygame
+from os import listdir
+import time
 
 from pygame.constants import QUIT, K_DOWN, K_UP, K_LEFT, K_RIGHT
 
 pygame.init()
 FPS = pygame.time.Clock()
 
-screen = width, height = 800, 600
+screen = width, height = 1000, 800
 
 BLACK = 0, 0, 0
 WHITE = 255, 255, 255
@@ -15,18 +17,16 @@ RED = 255, 0, 0
 CHANGING = random.randint(0, 255), random.randint(0, 255), random.randint(0, 255)
 
 font = pygame.font.SysFont('Verdana', 20)
+font_gameOver = pygame.font.SysFont('Verdana', 40)
 
-main_surface = pygame.display.set_mode(screen)
+main_surface = pygame.display.set_mode(screen) #creating display
+
+IMGS_PATH = 'Goose'
 
 #player = pygame.Surface((20, 20))
 #player.fill(WHITE)
-player_imgs = [pygame.image.load('1-1.png').convert_alpha(),
-    pygame.image.load('1-2.png').convert_alpha(),
-    pygame.image.load('1-3.png').convert_alpha(),
-    pygame.image.load('1-4.png').convert_alpha(),
-    pygame.image.load('1-5.png').convert_alpha()
-]
-player = player_imgs[1]
+player_imgs = [pygame.transform.scale_by(pygame.image.load(IMGS_PATH+'/'+file).convert_alpha(), 0.8) for file in listdir(IMGS_PATH)]
+player = player_imgs[0]
 player_rect = player.get_rect()
 player_speed = 5
 
@@ -34,7 +34,7 @@ player_speed = 5
 def create_enemy():
     #enemy = pygame.Surface((20,20))
     #enemy.fill(RED)
-    enemy = pygame.image.load('enemy.png').convert_alpha()
+    enemy = pygame.transform.scale_by(pygame.image.load('enemy.png').convert_alpha(), 0.8)
     enemy_rect = pygame.Rect(width, random.randint(0 + enemy.get_height(), height - enemy.get_height()), *enemy.get_size())
     enemy_speed = random.randint(2, 5)
     return [enemy, enemy_rect, enemy_speed]
@@ -43,8 +43,8 @@ def create_enemy():
 def create_bonus():
     #bonus = pygame.Surface((20,20))
     #bonus.fill(GREEN)
-    bonus = pygame.image.load('bonus.png').convert_alpha()
-    bonus_rect = pygame.Rect(random.randint(0 + bonus.get_width(), width - bonus.get_width()), 0, *bonus.get_size())
+    bonus = pygame.transform.scale_by(pygame.image.load('bonus.png').convert_alpha(), 0.8)
+    bonus_rect = pygame.Rect(random.randint(0 + bonus.get_width(), width - bonus.get_width()), -bonus.get_height(), *bonus.get_size())
     bonus_speed = random.randint(2, 5)
     return [bonus, bonus_rect, bonus_speed]
 
@@ -65,6 +65,9 @@ pygame.time.set_timer(CREATE_BONUS, 3500)
 CHANGE_IMG = pygame.USEREVENT + 3
 pygame.time.set_timer(CHANGE_IMG, 125)
 
+# GAME_OVER = pygame.USEREVENT + 4
+# pygame.time.set_timer(GAME_OVER, 125, 0)
+
 img_index = 0
 
 scores = 0
@@ -80,6 +83,7 @@ while is_working:
 
     for event in pygame.event.get():
         if event.type == QUIT:
+            pygame.quit()
             is_working = False
         if event.type == CREATE_ENEMY:
             enemies.append(create_enemy())
@@ -90,6 +94,7 @@ while is_working:
             if img_index == len(player_imgs):
                 img_index = 0
             player = player_imgs[img_index]
+
 
     pressed_keys = pygame.key.get_pressed()
 
@@ -108,7 +113,7 @@ while is_working:
     main_surface.blit(bg, (bgX, 0))
     main_surface.blit(bg, (bgX2, 0))
 
-    main_surface.blit(player, player_rect)
+    main_surface.blit(player, player_rect) #draw player on the display
 
     main_surface.blit(font.render(str(scores), True, BLACK), (width - 30, 0))
 
@@ -120,6 +125,17 @@ while is_working:
             enemies.pop(enemies.index(enemy))
 
         if player_rect.colliderect(enemy[1]):
+            # main_surface.fill(BLACK)
+            # event.type = GAME_OVER
+            # Якщо зіткнення сталось, то виводимо на екран "GAME OVER" та чекаємо 5 секунд
+            # font = pygame.font.Font(None, 100)
+            text = font_gameOver.render("GAME OVER", True, WHITE)
+            main_surface.fill(BLACK)
+            main_surface.blit(text, (width/2 - text.get_width()/2, height/2 - text.get_height()/2))
+            pygame.display.flip()
+            time.sleep(3)
+            # pygame.time.set_timer(False, 125)
+            pygame.quit()
             is_working = False
 
     for bonus in bonuses:
